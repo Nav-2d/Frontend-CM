bcb79805ee2fbdbfd22622fc668004cf5b118dd3
Navdeep Singh <navdeep.singh2088@gmail.com>
2019-10-22 23:16:26 -0700

    navbar implementation

--- CM/js/script.js
+++ CM/js/script.js
@@ -1,11 +1,4 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
-
-signUpButton.addEventListener('click', () => {
-    container.classList.add('right-panel-active')
-});
-
-signInButton.addEventListener('click', () => {
-    container.classList.remove('right-panel-active')
-});
+$(".icon").click(function() {
+    $(this).toggleClass("open");
+  });
+  
